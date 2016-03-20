3f029ac8a3734cf6cc494b095c8fbfd148aa8b3b
David J Felix <felix.davidj@gmail.com>
2016-03-20 19:52:57 -0400

    Add description to the string format

--- instameals/models/allergen.py
+++ instameals/models/allergen.py
@@ -12,7 +12,7 @@ class Allergen(models.Model):
         app_label = 'instameals'
 
     def __str__(self):
-        return "[{id}]: {name}".format(
+        return "[{id}]: {name}, {description}".format(
                 id=str(self.id),
                 name=str(self.name),
                 description=str(self.description)
