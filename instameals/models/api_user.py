5b02f920261b2a23dc031e7a2993c35e056dcd07
David J Felix <felix.davidj@gmail.com>
2016-03-22 21:05:40 -0400

    Hook up user login

--- instameals/models/api_user.py
+++ instameals/models/api_user.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from model_utils.models import TimeStampedModel
 
 from .uuid import UUIDModelMixin
 
 
-class APIUser(UUIDModelMixin, AbstractUser):
+class APIUser(UUIDModelMixin, TimeStampedModel, AbstractUser):
     class Meta:
         app_label = 'instameals'
