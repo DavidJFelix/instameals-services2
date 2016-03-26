45afcf6497421950755fdc1a2f1ea7a46d4fbb02
David J Felix <felix.davidj@gmail.com>
2016-03-26 14:31:31 -0400

    Filter meal list by active

--- instameals/views/meal.py
+++ instameals/views/meal.py
@@ -1,4 +1,5 @@
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 
 from .base import NoDeleteModelViewSet
 from ..models import Meal
@@ -9,3 +10,15 @@ class MealViewSet(NoDeleteModelViewSet):
     queryset = Meal.objects.all()
     serializer_class = MealSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        longitude = self.request.query_params.get('lng', None)
+        latitude = self.request.query_params.get('lat', None)
+        max_range = self.request.query_params.get('range', None)
+        limit = self.request.query_params.get('limit', None)
+        page = self.request.query_params.get('page', None)
+
+        meals = queryset.filter(is_active=True)
+        serializer = MealSerializer(meals, many=True, context={'request': request})
+        return Response(serializer.data)
